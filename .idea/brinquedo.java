f477f52a62e6bd0d64495ace178583b13790a519
Falconsfly <ninjapaquiderme@gmail.com>
2020-10-21 11:58:57 -0300

    Fiz a função de avaliação, mas sei lá... Tá muito simples pra ser verdade, entende? Não entendi direito como vamos representar o individuo/solução ainda, se vai ser um objeto ou como que ele vai ser um vector de inteiros e ao mesmo tempo referenciar a um objeto brinquedo e tal. Enfim, isso aí foi mais eu tentando testar a parada.

---
+++ .idea/brinquedo.java
@@ -0,0 +1,18 @@
+public class Brinquedo {
+    int diversao;
+    int tempo;
+
+    public int getDiversao() {
+        return diversao;
+    }
+    public int getTempo() {
+        return tempo;
+    }
+    public void setDiversão(int newDiversao) {
+        diversao = newDiversao;
+    }
+    public void setTempo(int newTempo) {
+        tempo = newTempo;
+    }
+
+}
